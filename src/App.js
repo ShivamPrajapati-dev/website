67c096b5ea99524d216c888d91aab40b59a528d3
Shivam Prajapati <shivamprajapati.git@gmail.com>
2019-12-25 02:21:50 +0530

    updates

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Lottie from 'react-lottie';
 import  animationData from './wave.json';
 import logo from './nik.jpg';
 import {Image, Header, Grid, Icon} from 'semantic-ui-react';
-import { MDBIcon, MDBContainer, MDBBtn } from 'mdbreact';
+import { MDBCol, MDBContainer, MDBRow, MDBFooter } from "mdbreact";
 import git from './github-logo.png';
 import linkedin from './linkedin.png';
 import resume from './resume.png';
@@ -26,7 +26,7 @@ class App extends React.Component {
     };
 
     return (
-      <div>
+      <div fluid>
         <Lottie options={defaultOptions}
           height={200} />
         <Image className="img-fluid z-depth-3" alt="" src={logo} size='small' circular centered />
@@ -35,33 +35,34 @@ class App extends React.Component {
            <Header.Subheader>Live life to the fullest...</Header.Subheader>
         </Header>
         <div style={{
-            marginTop:'50px'
+            marginTop:'100px'
           }} centered>
         <Grid centered>
         <Grid.Row centered>
           <Grid.Column>
             <a>
-            <Image className="img-fluid z-depth-3" circular src={git} size='tiny'/>
+            <Image className="img-fluid z-depth-2" circular src={git} size='mini'/>
             </a>
           </Grid.Column>
           <Grid.Column>
             <a>
-              <Image className="img-fluid z-depth-3" circular src={linkedin} size='tiny'/>
+              <Image className="img-fluid z-depth-2" circular src={linkedin} size='mini'/>
             </a>
           </Grid.Column>
           <Grid.Column>
             <a>
-            <Image className="img-fluid z-depth-3" circular src={resume} size='tiny'/>
+            <Image className="img-fluid z-depth-2" circular src={resume} size='mini'/>
             </a>
           </Grid.Column>
           <Grid.Column>
             <a>
-              <Image className="img-fluid z-depth-3" circular src={email} size='tiny'/>
+              <Image className="img-fluid z-depth-2" circular src={email} size='mini'/>
             </a>
             </Grid.Column>
         </Grid.Row>
       </Grid>
     </div>
+
       </div>
 
     );
