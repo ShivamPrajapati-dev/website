2f0b86c1d10c988f55cbd48cffe18779523b98da
Shivam Prajapati <shivamprajapati.git@gmail.com>
2019-12-25 01:32:05 +0530

    created front page

--- src/App.js
+++ src/App.js
@@ -1,26 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Lottie from 'react-lottie';
+import  animationData from './wave.json';
+import logo from './nik.jpg';
+import {Image, Header, Grid, Icon} from 'semantic-ui-react';
+import { MDBIcon, MDBContainer, MDBBtn } from 'mdbreact';
+import git from './github-logo.png';
+import linkedin from './linkedin.png';
+import resume from './resume.png';
+import email from './email.png';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+
+
+class App extends React.Component {
+  render(){
+
+    const defaultOptions = {
+      loop: true,
+      autoplay: true,
+      animationData: animationData,
+      rendererSettings: {
+        preserveAspectRatio: 'xMidYMid slice'
+      }
+    };
+
+    return (
+      <div>
+        <Lottie options={defaultOptions}
+          height={200} />
+        <Image className="img-fluid z-depth-3" alt="" src={logo} size='small' circular centered />
+        <Header as='h1' textAlign='center'>
+          <Header.Content>Shivam Prajapati</Header.Content>
+           <Header.Subheader>Live life to the fullest...</Header.Subheader>
+        </Header>
+        <div style={{
+            marginTop:'50px'
+          }} centered>
+        <Grid centered>
+        <Grid.Row centered>
+          <Grid.Column>
+            <a>
+            <Image className="img-fluid z-depth-3" circular src={git} size='tiny'/>
+            </a>
+          </Grid.Column>
+          <Grid.Column>
+            <a>
+              <Image className="img-fluid z-depth-3" circular src={linkedin} size='tiny'/>
+            </a>
+          </Grid.Column>
+          <Grid.Column>
+            <a>
+            <Image className="img-fluid z-depth-3" circular src={resume} size='tiny'/>
+            </a>
+          </Grid.Column>
+          <Grid.Column>
+            <a>
+              <Image className="img-fluid z-depth-3" circular src={email} size='tiny'/>
+            </a>
+            </Grid.Column>
+        </Grid.Row>
+      </Grid>
     </div>
-  );
+      </div>
+
+    );
+  }
 }
 
 export default App;
